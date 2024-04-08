4340118331fddcf31099d4f25b5316411fbfeae1
Diego Bernardi <diegobernardisza@gmail.com>
2024-04-08 10:42:28 -0300

    add accounts login/signup/logout views and templates

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from allauth.account.views import SignupView, LoginView, LogoutView
 
 
 urlpatterns = [
-    path('cadastro/', SignupView.as_view(), name='signup'),
-    path('', LoginView.as_view(), name='login'),
+    path('cadastro/', SignupView.as_view(template_name = 'signup.html'), name='signup'),
+    path('', LoginView.as_view(template_name ='signin.html'), name='login'),
     path('sair/', LogoutView.as_view(), name='logout'),
 ]
