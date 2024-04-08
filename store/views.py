4c589f3c845c7b91d4ec0d928632d03b9b175cf7
Diego Bernardi <diegobernardisza@gmail.com>
2024-04-08 02:20:15 -0300

    add product model and update templates

--- store/views.py
+++ store/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
 from django.views.generic import TemplateView
+
+from .models import Product
 
 
 class StoreIndexView(TemplateView):
     template_name = 'store/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['products'] = Product.objects.all()
+        return context
