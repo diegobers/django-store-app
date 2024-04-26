92172a2b4825fdb4ea2e242267d400df4b3e44ca
Diego Bernardi <diegobernardisza@gmail.com>
2024-04-26 17:16:01 -0300

    update anonymous user can store cart_items

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 
-from allauth.account.views import SignupView, LoginView, LogoutView
+from allauth.account.views import SignupView, LogoutView
+from accounts.views import CustomLoginView
+
 
 app_name = 'accounts'
 
 
 urlpatterns = [
     path('cadastro/', SignupView.as_view(template_name = 'signup.html'), name='signup'),
-    path('', LoginView.as_view(template_name ='signin.html'), name='login'),
+    path('', CustomLoginView.as_view(), name='login'),
     path('sair/', LogoutView.as_view(), name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from allauth.account.views import LoginView
+from django.urls import reverse_lazy
+from store.models import Cart
 
-# Create your views here.
+
+class CustomLoginView(LoginView):
+    template_name = 'signin.html'
+    success_url = reverse_lazy('store:view_cart') 
+    
+    def get(self, request, *args, **kwargs):
+        request.session['last_session_key'] = request.session.session_key
+
+        return super().get(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        
+        last_session_key = self.request.session.get('last_session_key')
+        cart_queryset = Cart.objects.filter(session_key=last_session_key)
+        
+        if cart_queryset.exists():
+            cart = cart_queryset.first()
+            cart.user_id = self.request.user.id
+            cart.session_key = None
+            cart.save()
+            del self.request.session['last_session_key']
+        return response
